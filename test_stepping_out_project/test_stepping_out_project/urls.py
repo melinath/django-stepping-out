15165769f93d122ac383d861508034f72b5d58dd
Stephen Burrows <stephen.r.burrows@gmail.com>
2013-01-23 04:40:21 -0800

    Added basic bootstrap usage.

--- test_stepping_out_project/test_stepping_out_project/urls.py
+++ test_stepping_out_project/test_stepping_out_project/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 
@@ -9,4 +11,4 @@ urlpatterns = patterns('',
     url(r'^$', include('stepping_out.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
